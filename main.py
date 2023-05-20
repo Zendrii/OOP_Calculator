5d7daf862cb8f69fe5920792379b00a202713628
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-05-20 18:32:56 +0800

    Importation of functions

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # import functions from class
+from calculator_class import Calculator
+
 # ask user for operation to be used
+
 # calculate inputs using funtions from class
 # check for errors
 # output result
