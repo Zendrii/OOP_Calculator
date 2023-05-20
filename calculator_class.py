323ae2a8ff875b269b0a88d96be06d51b366aef1
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-05-20 18:21:09 +0800

    Addition function creation

--- calculator_class.py
+++ calculator_class.py
@@ -1,9 +1,12 @@
 # create class
 class Calculator:
-        def __init__(self):
-            pass
-        
-# create behaviors/statements
+    def __init__(self):
+        pass
+
+    # create behaviors/statements
+    def add(self, num1, num2):
+        return float(num1) + float(num2)
+    
 # check for errors
 # ask user for operation to be used
 # calculate inputs using funtions from class
