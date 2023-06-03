1f802d63925caf854ba9d74ae395e714cebd6fa3
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-06-03 16:58:42 +0800

    Finalization of functionality of main file

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from child_class import Calculation
+def main():
+    from child_class import Calculation
 
-clclt = Calculation()
-clclt
+    clclt = Calculation()
+    
+    clclt.clcltn()
 
-# Ask if the user wants to try again or not
-choice = input('\nDo you want to try again? y/n  ')
+while True:
+    main()
 
-# if yes, restart the program
-if choice == 'y':
-    clclt
-# If no, the program will exit
-elif choice == 'n':
-    print('\nThank you!')
+    # Ask if the user wants to try again or not
+    choice = input('\nDo you want to try again? y/n  ')
 
-    print('\n', '\033[92m='*150)
+    # If no, the program will exit
+    if choice == 'n':
+        print('\nThank you!')
+
+        print('\n', '\033[92m='*150)
+        
+        break
