3b62b7411a7d5da2b3173f183301e3815792b28c
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-06-03 16:26:16 +0800

    Still trying to call objects from subclass 1

--- child_class.py
+++ child_class.py
@@ -6,37 +6,40 @@ class Objects:
         # create objects to use
         calc = Calculator()
 
+    def oprtn(self):
         # ask user for operation to be used
         operation = input('\nEnter operation: [ + | - | * | / ] ')
 
+    def npts(self):
         # ask user for numbers to be used
         num1 = input('\nEnter 1st number: ')
         num2 = input('\nEnter 2nd number: ')
 
 # add/create a new class that inherits the functions from parent class
 class Conditions(Objects):
-    try:
-        oper = Objects().objcts 
-        # calculate inputs using funtions from class
-        if operation == '+':
-            sum = calc.addition(num1, num2)
-            # output result
-            print('\n', sum)
-        elif operation == '-':
-            dffrnc = calc.subtraction(num1, num2)
-            # output result
-            print('\n', dffrnc)
-        elif operation == '*':
-            prdct = calc.multiplication(num1, num2)
-            # output result
-            print('\n', prdct)
-        elif operation == '/':
-            qtnt = calc.division(num1, num2)
-            # output result
-            print('\n', qtnt)
+    def chckr(self):
+        try:
+            oper = Objects().objcts 
+            # calculate inputs using funtions from class
+            if operation == '+':
+                sum = calc.addition(num1, num2)
+                # output result
+                print('\n', sum)
+            elif operation == '-':
+                dffrnc = calc.subtraction(num1, num2)
+                # output result
+                print('\n', dffrnc)
+            elif operation == '*':
+                prdct = calc.multiplication(num1, num2)
+                # output result
+                print('\n', prdct)
+            elif operation == '/':
+                qtnt = calc.division(num1, num2)
+                # output result
+                print('\n', qtnt)
 
-    # check for errors
-    except ValueError:
-        print('\nOnly Numbers allowed!')
-    except TypeError:
-        print('\nAn Error has Occured.')
+        # check for errors
+        except ValueError:
+            print('\nOnly Numbers allowed!')
+        except TypeError:
+            print('\nAn Error has Occured.')
