3cfb1e20ccf4b030badef2438098d4acb391045c
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-06-03 16:05:01 +0800

    Creation of subclass 1 within the child_class file

--- child_class.py
+++ child_class.py
@@ -1,4 +1,15 @@
-# import functions from parent class
-from calculator_class import Calculator
+class Objects:
+    # import functions from parent class
+    from calculator_class import Calculator
+
+    # create objects to use
+    calc = Calculator()
+
+    # ask user for operation to be used
+    operation = input('\nEnter operation: [ + | - | * | / ] ')
+
+    # ask user for numbers to be used
+    num1 = input('\nEnter 1st number: ')
+    num2 = input('\nEnter 2nd number: ')
 
 # add/create a new class that inherits the functions from parent class
