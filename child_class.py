e0a150d2a00736f44c0edb443c8d4134d2807e17
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-06-03 16:14:50 +0800

    Trying to call the objects from subclass 1

--- child_class.py
+++ child_class.py
@@ -1,20 +1,22 @@
 class Objects:
-    # import functions from parent class
-    from calculator_class import Calculator
+    def objcts(self):
+        # import functions from parent class
+        from calculator_class import Calculator
 
-    # create objects to use
-    calc = Calculator()
+        # create objects to use
+        calc = Calculator()
 
-    # ask user for operation to be used
-    operation = input('\nEnter operation: [ + | - | * | / ] ')
+        # ask user for operation to be used
+        operation = input('\nEnter operation: [ + | - | * | / ] ')
 
-    # ask user for numbers to be used
-    num1 = input('\nEnter 1st number: ')
-    num2 = input('\nEnter 2nd number: ')
+        # ask user for numbers to be used
+        num1 = input('\nEnter 1st number: ')
+        num2 = input('\nEnter 2nd number: ')
 
 # add/create a new class that inherits the functions from parent class
-class Conditions:
+class Conditions(Objects):
     try:
+        oper = Objects().objcts 
         # calculate inputs using funtions from class
         if operation == '+':
             sum = calc.addition(num1, num2)
