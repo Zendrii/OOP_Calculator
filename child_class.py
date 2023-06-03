3024ed6cae246c7827ce4c60a1264d74010353db
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-06-03 16:07:10 +0800

    Creation of subclass 2 within file

--- child_class.py
+++ child_class.py
@@ -13,3 +13,28 @@ class Objects:
     num2 = input('\nEnter 2nd number: ')
 
 # add/create a new class that inherits the functions from parent class
+class Conditions:
+    try:
+        # calculate inputs using funtions from class
+        if operation == '+':
+            sum = calc.addition(num1, num2)
+            # output result
+            print('\n', sum)
+        elif operation == '-':
+            dffrnc = calc.subtraction(num1, num2)
+            # output result
+            print('\n', dffrnc)
+        elif operation == '*':
+            prdct = calc.multiplication(num1, num2)
+            # output result
+            print('\n', prdct)
+        elif operation == '/':
+            qtnt = calc.division(num1, num2)
+            # output result
+            print('\n', qtnt)
+
+    # check for errors
+    except ValueError:
+        print('\nOnly Numbers allowed!')
+    except TypeError:
+        print('\nAn Error has Occured.')
