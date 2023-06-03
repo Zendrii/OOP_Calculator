034a3a1397a6913a210a0b62cc34ab698b3cb6db
Dan Zendrich H. Reyes <danzendrichreyes04@gmail.com>
2023-06-03 16:34:02 +0800

    Stil trying to call functions from subclass 1

--- child_class.py
+++ child_class.py
@@ -1,27 +1,35 @@
 class Objects:
-    def objcts(self):
+    def objct(self):
         # import functions from parent class
         from calculator_class import Calculator
 
         # create objects to use
         calc = Calculator()
+        return calc
 
     def oprtn(self):
         # ask user for operation to be used
         operation = input('\nEnter operation: [ + | - | * | / ] ')
+        return operation
 
     def npts(self):
         # ask user for numbers to be used
         num1 = input('\nEnter 1st number: ')
         num2 = input('\nEnter 2nd number: ')
+        return num1, num2
 
 # add/create a new class that inherits the functions from parent class
 class Conditions(Objects):
     def chckr(self):
         try:
-            oper = Objects().objcts 
+            obj = super().objct()
+            oper = super().oprtn()
+            inp = super().npts()
+
+            
+
             # calculate inputs using funtions from class
-            if operation == '+':
+            if oper == '+':
                 sum = calc.addition(num1, num2)
                 # output result
                 print('\n', sum)
